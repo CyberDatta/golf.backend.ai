fe7959210b7f8c0430e4b507f2ff0dfdd24dd94e
karan3613 <karanchauhan.3613@gmail.com>
2024-11-27 17:06:32 +0530

    first commit

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from exceptiongroup import catch
-from fastapi import FastAPI , HTTPException
+
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
+
+
+class LandmarksModel(BaseModel):
+    x : float
+    y : float
+    z : float
+    v : float
+    p : float
+
+
 class DetectionModel(BaseModel):
-    frames : list
-    landmarks : list
+    frames: list[list[float]]
+    landmarks: list[list[float]]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,16 +31,16 @@ app.add_middleware(
 @app.get("/")
 async def home():
     return {
-        "MESSAGE" : "START OF API "
+        "MESSAGE": "START OF API "
     }
 
-@app.post("/detection")
-async def post_detection(detection : DetectionModel):
-    try :
-        return {
-            'frames' : detection.frames ,
-            'landmarks' : detection.landmarks
-        }
-    except HTTPException :
-        return HTTPException(detail= "the list is Empty" , status_code=404)
 
+@app.post("/detection")
+async def post_detection(detection: DetectionModel):
+    try:
+        return {
+            'frames': detection.frames,
+            'landmarks': detection.landmarks
+        }
+    except HTTPException:
+        return HTTPException(detail="the list is Empty", status_code=404)
